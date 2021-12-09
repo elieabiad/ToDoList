19a3805e2c1883a54efff5abeaf8f540bfbbc4d6
Elie Abiad <elie.abiad@hotmail.com>
2021-12-09 22:41:38 +0200

    testing first commit

--- src/main/java/com/elieabiad/toDoList/advice/LoggingAdvice.java
+++ src/main/java/com/elieabiad/toDoList/advice/LoggingAdvice.java
@@ -5,13 +5,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
+
 
 @Aspect
 @Component
