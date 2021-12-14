2ec08027851a458ce7d85dcac170588472bc9e8a
elie.abiad@hotmail.com <elie.abiad@hotmail.com>
2021-12-14 22:29:40 +0200

    adding the csrf token cookie

--- src/main/java/com/elieabiad/toDoList/config/SecurityConfig.java
+++ src/main/java/com/elieabiad/toDoList/config/SecurityConfig.java
@@ -10,15 +10,22 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    protected void configure(HttpSecurity http) throws Exception {
+/*    protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
                 .authorizeRequests()
                 .anyRequest().permitAll();//disabled csrf because when it was enabled I couldn't send put delete create requests
         http.headers().frameOptions().disable();//disabled headers to connect to H2 console: https://stackoverflow.com/questions/53395200/h2-console-is-not-showing-in-browser
+    }*/
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()));
+        http.logout(logout -> logout.logoutRequestMatcher(new AntPathRequestMatcher("/logout")));
     }
     @Bean
     @Override
